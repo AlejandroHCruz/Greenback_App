cb66b6894fa54e04dceaa55b759a2bd366b04e30
Jorge <jorgeduque71@gmail.com>
2013-11-12 00:56:44 -0600

    Styling MVP 11 nov

--- src/co/ensalsaverde/apps/greenback/Fragment_3.java
+++ src/co/ensalsaverde/apps/greenback/Fragment_3.java
@@ -9,7 +9,7 @@ import android.view.ViewGroup;
 public class Fragment_3 extends Fragment{
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-		return inflater.inflate(R.layout.fragment_3, null);
+		return inflater.inflate(R.layout.underdevelopment, null);
 	}
 
 }
